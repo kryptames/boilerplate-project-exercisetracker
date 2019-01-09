88322413d685c29a5ed0760cbdc28105d1513e0f
Glitch (apis-and-microservices-projects-exercise-tracker) <none>
2019-01-09 11:45:04 +0000

    query date, limit

--- server.js
+++ server.js
@@ -21,34 +21,56 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// Not found middleware
-// app.use((req, res, next) => {
-//   return next({status: 404, message: 'not found'})
-// })
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
-app.get('/api/exercise/log', function(req,res){
-  User.find({_id: req.query.userId}, function(err, data){
-    res.send(data)
+app.get('/api/exercise/log', function(req,res,next){
+  console.log(req.query)
+  User.findById(req.query.userId)
+  .exec(function(err, data){
+    if (err) next();
+    var out = {
+      _id: data._id,
+      username: data.username,
+      count:0,
+      log:[]
+    }
+    var num=0;
+    var query;
+    var from = new Date(req.query.from)
+    var to = new Date(req.query.to)
+    data.log.forEach(function(element) {
+      if (num<(req.query.limit || data.log.length)) {
+        if (req.query.from && req.query.to) query=element.date>=from && element.date<to;
+        else if (req.query.from) query=element.date>=from;
+        else if (req.query.to) query=element.date<to;
+        else query=1;
+        console.log(element)
+        if (query){
+          num++;
+          out.count = num;
+          out.log.push({description: element.description,
+                       duration: element.duration,
+                       date: element.date.toDateString()});
+          }
+      }
+    })
+    res.send(out)
   })
+  // User.aggregate([
+  //   {
+  //     $match: {_id: req.query.userId
+  //              // "log.date": {$gte: req.query.from, $lt: req.query.to}
+  //             }
+  //   },
+  //   { 
+  //     $project: { _id: "$_id", 
+  //                username: "$username",
+  //                // count: {$sum:1},
+  //                log: { $slice: [ "$log", req.query.limit ] } 
+  //               }
+  //   } 
+  // ]).exec(function(err, data){
+  //   if (err) next();
+  //   res.send(data)
+  // })
 })
 
 app.post('/api/exercise/new-user', function(req,res){
@@ -94,6 +116,30 @@ app.post('/api/exercise/add', function(req,res){
     }
   })
   }
+})
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
